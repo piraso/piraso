f0a952aa8c5c7675903b9586eba22cd8c5b9d3e1
Alvin R. de Leon <adleon@adleon-laptop.(none)>
2011-10-21 18:22:28 +0800

    force to compile to version 1.5

---
+++ context-logger/server/src/test/java/ard/piraso/server/proxy/RegexProxyFactoryTest.java
@@ -0,0 +1,24 @@
+package ard.piraso.server.proxy;
+
+import org.junit.Test;
+
+/**
+ * Test for {@link RegexProxyFactory} class.
+ */
+public class RegexProxyFactoryTest {
+
+    @Test
+    public void testGetProxy() throws Exception {
+
+    }
+
+    @Test
+    public void testGetProxyInterceptor() throws Exception {
+
+    }
+
+    @Test
+    public void testAddMethodListener() throws Exception {
+
+    }
+}
