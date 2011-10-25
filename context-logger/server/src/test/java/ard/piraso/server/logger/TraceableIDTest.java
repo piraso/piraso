664b44e76c19486101c5847a33eb358fe3702e72
Alvin R. de Leon <adleon@adleon-laptop.(none)>
2011-10-25 17:21:28 +0800

    increase code coverage.

--- context-logger/server/src/test/java/ard/piraso/server/logger/TraceableIDTest.java
+++ context-logger/server/src/test/java/ard/piraso/server/logger/TraceableIDTest.java
@@ -2,8 +2,7 @@ package ard.piraso.server.logger;
 
 import org.junit.Test;
 
-import static junit.framework.Assert.assertEquals;
-import static junit.framework.Assert.assertFalse;
+import static junit.framework.Assert.*;
 
 /**
  * Test for {@link TraceableID} class.
@@ -30,5 +29,41 @@ public class TraceableIDTest {
 
         assertEquals("test_2", id2.getIds().getLast());
         assertEquals("test_3", id3.getIds().getLast());
+    }
+
+    @Test
+    public void testCreateWithIntArg() throws Exception {
+        TraceableID id = new TraceableID("test");
+        TraceableID id2 = id.create(1);
+
+        assertEquals(2, id2.getIds().size());
+        assertEquals("[test, 1]", id2.toString());
+    }
+
+    @Test
+    public void testCreateWithStringArg() throws Exception {
+        TraceableID id = new TraceableID("test");
+        TraceableID id2 = id.create("test2");
+
+        assertEquals(2, id2.getIds().size());
+        assertEquals("[test, test2]", id2.toString());
+    }
+
+    @Test
+    public void testCreateWithStringAndHashCodeArg() throws Exception {
+        TraceableID id = new TraceableID("test");
+        TraceableID id2 = id.create("test2", "test2".hashCode());
+
+        assertEquals(2, id2.getIds().size());
+        assertEquals("[test, test2" + Integer.toHexString("test2".hashCode()) + "]", id2.toString());
+    }
+
+    @Test
+    public void testProperty() throws Exception {
+        TraceableID id = new TraceableID("test");
+        id.addProperty(String.class, "hello");
+
+        assertEquals("hello", id.getProperty(String.class));
+        assertTrue(id.hasProperty(String.class));
     }
 }
