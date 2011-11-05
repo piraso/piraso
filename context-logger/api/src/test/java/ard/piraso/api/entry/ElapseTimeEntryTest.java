19c5d0845c67e27e049d7422fa0e47b5a9a72df1
Alvin <alvinrdeleon@me.com>
2011-11-06 01:50:55 +0800

    added license and copyright.

--- context-logger/api/src/test/java/ard/piraso/api/entry/ElapseTimeEntryTest.java
+++ context-logger/api/src/test/java/ard/piraso/api/entry/ElapseTimeEntryTest.java
@@ -1,6 +1,25 @@
+/*
+ * Copyright (c) 2011. Piraso Alvin R. de Leon. All Rights Reserved.
+ *
+ * See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The Piraso licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package ard.piraso.api.entry;
 
 import ard.piraso.api.AbstractJacksonTest;
+import ard.piraso.api.Level;
 import org.junit.Test;
 
 import java.io.IOException;
@@ -65,6 +84,7 @@ public class ElapseTimeEntryTest extends AbstractJacksonTest {
         long startTime = System.currentTimeMillis();
         long endTime = System.currentTimeMillis() + elapseTime;
         ElapseTimeEntry expected = new ElapseTimeEntry(startTime, endTime);
+        expected.setLevel(Level.ALL.getName());
 
         String jsonValue = mapper.writeValueAsString(expected);
         ElapseTimeEntry actual = mapper.readValue(jsonValue, ElapseTimeEntry.class);
