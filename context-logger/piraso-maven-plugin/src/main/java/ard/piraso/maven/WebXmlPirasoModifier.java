96b23bc0ce0dd10d9626ea01b5582224c98a5d21
Alvin <alvinrdeleon@me.com>
2012-06-03 21:24:49 +0800

    added web xml modifier maven plugin.

---
+++ context-logger/piraso-maven-plugin/src/main/java/ard/piraso/maven/WebXmlPirasoModifier.java
@@ -0,0 +1,182 @@
+/*
+ * Copyright (c) 2012. Piraso Alvin R. de Leon. All Rights Reserved.
+ *
+ * See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The Piraso licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package ard.piraso.maven;
+
+import com.sun.org.apache.xerces.internal.parsers.DOMParser;
+import org.apache.maven.plugin.AbstractMojo;
+import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugin.MojoFailureException;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.xml.sax.InputSource;
+
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerException;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.FileReader;
+
+/**
+ * Web xml Piraso modifier
+ *
+ * @goal web-xml
+ */
+public class WebXmlPirasoModifier  extends AbstractMojo {
+
+    /**
+     * piraso logging path
+     *
+     * @parameter default-value="/piraso/logging"
+     */
+    private String pirasoLoggingPath;
+
+    /**
+     * the web.xml file to modify
+     *
+     * @parameter default-value="${maven.war.webxml}"
+     */
+    private File webXml;
+
+    /**
+     * output directed
+     *
+     * @parameter default-value="${project.build.directory}/piraso/WEB-INF/"
+     */
+    private File outputDirectory;
+
+    private Document document;
+
+
+    public void setOutputDirectory(File outputDirectory) {
+        this.outputDirectory = outputDirectory;
+    }
+
+    public void setPirasoLoggingPath(String pirasoLoggingPath) {
+        this.pirasoLoggingPath = pirasoLoggingPath;
+    }
+
+    public void setWebXml(File webXml) {
+        this.webXml = webXml;
+    }
+
+    /**
+     * Start modifying the web.xml
+     * @throws MojoExecutionException on error
+     * @throws MojoFailureException on error
+     */
+    public void execute() throws MojoExecutionException, MojoFailureException {
+        parseDocument();
+
+        Node root = document.getElementsByTagName("web-app").item(0);
+        Node firstContextParam = document.getElementsByTagName("context-param").item(0);
+        Node firstFilter = document.getElementsByTagName("filter").item(0);
+
+        insertContextParam(root, firstContextParam, "parentContextKey", "piraso.context");
+        insertContextParam(root, firstContextParam, "contextClass", "ard.piraso.server.spring.web.PirasoWebApplicationContext");
+        insertFilterAndServletElement(root, firstFilter);
+
+        try {
+            writeDocument();
+        } catch (Exception e) {
+            throw new MojoExecutionException(e.getMessage(), e);
+        }
+    }
+
+    private void writeDocument() throws TransformerException, FileNotFoundException {
+        File output = new File(outputDirectory, "web.xml");
+
+        System.out.println("Output: " + output.toString());
+
+        TransformerFactory transformerFactory = TransformerFactory.newInstance();
+        Transformer transformer = transformerFactory.newTransformer();
+
+        StreamResult outputTarget = new StreamResult(new FileOutputStream(output));
+        DOMSource source = new DOMSource(document);
+        transformer.transform(source, outputTarget);
+    }
+
+    private void insertContextParam(Node root, Node insertBefore, String name, String value) {
+        Node buf = document.createTextNode("\n  ");
+        Node contextParam = createContextParam(name, value);
+        root.insertBefore(buf, insertBefore);
+        root.insertBefore(contextParam, buf);
+    }
+
+    private void insertFilterAndServletElement(Node root, Node insertBefore) {
+        Element filter = createElementNameValue("filter", "filter-name", "filter-class", "pirasoFilter", "org.springframework.web.filter.DelegatingFilterProxy");
+        Element filterMapping = createElementNameValue("filter-mapping", "filter-name", "url-pattern", "pirasoFilter", "/*");
+        Element servlet = createElementNameValue("servlet", "servlet-name", "filter-class", "pirasoServlet", "org.springframework.web.context.support.HttpRequestHandlerServlet");
+        Element servletMapping = createElementNameValue("servlet-mapping", "servlet-name", "url-pattern", "pirasoServlet", "/piraso/logging");
+
+        Node buf1 = document.createTextNode("\n  ");
+        Node buf2 = document.createTextNode("\n  ");
+        Node buf3 = document.createTextNode("\n  ");
+        Node buf4 = document.createTextNode("\n  ");
+
+        root.insertBefore(buf1, insertBefore);
+        root.insertBefore(servletMapping, buf1);
+
+        root.insertBefore(buf2, servletMapping);
+        root.insertBefore(servlet, buf2);
+
+        root.insertBefore(buf3, servlet);
+        root.insertBefore(filterMapping, buf3);
+
+        root.insertBefore(buf4, filterMapping);
+        root.insertBefore(filter, buf4);
+    }
+
+
+    private Element createContextParam(String name, String value) {
+        return createElementNameValue("context-param", "param-name", "param-value", name, value);
+    }
+
+    private Element createElementNameValue(String node, String nameNode, String valueNode, String name, String value) {
+        Element filter = document.createElement(node);
+        Element paramName = document.createElement(nameNode);
+        Element paramClass = document.createElement(valueNode);
+
+        paramName.setTextContent(name);
+        paramClass.setTextContent(value);
+
+        filter.appendChild(document.createTextNode("\n    "));
+        filter.appendChild(paramName);
+        filter.appendChild(document.createTextNode("\n    "));
+        filter.appendChild(paramClass);
+        filter.appendChild(document.createTextNode("\n  "));
+
+        return filter;
+    }
+
+    private void parseDocument() throws MojoExecutionException {
+        try {
+            DOMParser parser = new DOMParser();
+            parser.parse(new InputSource(new FileReader(webXml)));
+
+            document = parser.getDocument();
+        } catch (Exception e) {
+            throw new MojoExecutionException(e.getMessage(), e);
+        }
+    }
+}
---
+++ context-logger/piraso-maven-plugin/src/test/java/ard/piraso/maven/WebXmlPirasoModifierTest.java
@@ -0,0 +1,46 @@
+/*
+ * Copyright (c) 2012. Piraso Alvin R. de Leon. All Rights Reserved.
+ *
+ * See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The Piraso licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package ard.piraso.maven;
+
+import org.junit.Test;
+
+import java.io.File;
+
+/**
+ * Test for {@link WebXmlPirasoModifier} class.
+ */
+public class WebXmlPirasoModifierTest {
+    @Test
+    public void testExecute() throws Exception {
+        WebXmlPirasoModifier modifier = new WebXmlPirasoModifier();
+
+        File webXml = new File(WebXmlPirasoModifier.class.getResource("/web.xml").getFile());
+        File parent = new File(webXml.getParentFile(), "new");
+
+        if(!parent.isDirectory()) {
+            parent.mkdirs();
+        }
+
+        modifier.setPirasoLoggingPath("/piraso/logging");
+        modifier.setWebXml(webXml);
+        modifier.setOutputDirectory(parent);
+
+        modifier.execute();
+    }
+}
