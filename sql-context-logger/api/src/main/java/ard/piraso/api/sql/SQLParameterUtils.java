936c231b0507b3a23eb8818170608b1f09a11b2f
Alvin <alvinrdeleon@me.com>
2012-06-09 13:56:10 +0800

    minor changes on sql data viewing.

--- sql-context-logger/api/src/main/java/ard/piraso/api/sql/SQLParameterUtils.java
+++ sql-context-logger/api/src/main/java/ard/piraso/api/sql/SQLParameterUtils.java
@@ -1,3 +1,21 @@
+/*
+ * Copyright (c) 2012. Piraso Alvin R. de Leon. All Rights Reserved.
+ *
+ * See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The Piraso licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package ard.piraso.api.sql;
 
 import ard.piraso.api.entry.ObjectEntry;
@@ -155,7 +173,7 @@ public class SQLParameterUtils {
             int j = 1;
             for (List<SQLParameterEntry> row : entry.getRecords()) {
                 Vector<String> v = new Vector<String>();
-                v.add("@" + (j++) + "  " + StringUtils.repeat("-", 25));
+                v.add("@Row " + (j++));
                 v.add(StringUtils.repeat("-", 100));
                 data.add(v);
                 for (SQLParameterEntry param : row) {
