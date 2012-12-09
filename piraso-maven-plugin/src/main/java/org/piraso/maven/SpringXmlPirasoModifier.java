19a61edc378c99389e84820dee668d4624e7dbb8
Alvin R. de Leon <alvinrdeleon@me.com>
2012-12-09 14:02:34 +0800

    updated piraso maven plugin

--- piraso-maven-plugin/src/main/java/org/piraso/maven/SpringXmlPirasoModifier.java
+++ piraso-maven-plugin/src/main/java/org/piraso/maven/SpringXmlPirasoModifier.java
@@ -21,7 +21,6 @@ package org.piraso.maven;
 import org.apache.commons.lang.StringUtils;
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugin.MojoFailureException;
-import org.apache.maven.plugins.annotations.LifecyclePhase;
 import org.apache.maven.plugins.annotations.Mojo;
 import org.apache.maven.plugins.annotations.Parameter;
 import org.w3c.dom.Element;
